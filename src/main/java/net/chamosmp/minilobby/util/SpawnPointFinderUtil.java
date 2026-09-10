d71f4e5d314953cc399ab00a3265baa86211dec8
Chamogelastos <stavros.ssta@gmail.com>
2026-09-10 12:35:02 +0300

    Remove location caching

--- src/main/java/net/chamosmp/minilobby/util/SpawnPointFinderUtil.java
+++ src/main/java/net/chamosmp/minilobby/util/SpawnPointFinderUtil.java
@@ -6,7 +6,6 @@ import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 public class SpawnPointFinderUtil {
-    private Location location;
 
     private final Plugin plugin;
 
@@ -15,18 +14,14 @@ public class SpawnPointFinderUtil {
     }
 
     public @NotNull Location getLocation() {
-        if (location == null) {
-            FileConfiguration config = plugin.getConfig();
+        FileConfiguration config = plugin.getConfig();
 
-            Location loc = new Location(
-                    plugin.getServer().getRespawnWorld(),
-                    0.0,
-                    0.0,
-                    0.0
-            );
-
-            location = config.getObject("spawn-location", Location.class, loc);
-        }
-        return location;
+        Location loc = new Location(
+                plugin.getServer().getRespawnWorld(),
+                0.0,
+                0.0,
+                0.0
+        );
+        return config.getObject("spawn-location", Location.class, loc);
     }
 }
